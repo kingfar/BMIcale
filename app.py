150394160c3cd1360b2d80c763bbe3733fdbee4c
DESKTOP-HBA3O4D\kingfar <kingfar945@gmail.com>
2023-09-23 20:32:33 +0800

    Update app.py
    test 1  edit

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 height = int(input("please input ypur height"))
 weight = int(input("please input ypur weight"))
-lmi = weight / (height / 100)  ** 2
+bmi = weight / (height / 100)  ** 2
 print("your height = " + str(height)) 
+print("your weight = " + str(weight))
+print("your bmi =" + str(round(bmi,2)))
